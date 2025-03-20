75b1bcb2d460c9ecaf39c2a1379d97f032fdcaa6
IvanTham <Ivantham789@gmail.com>
2025-03-20 20:52:08 +0800

    Fix: hosting problem

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { getFirestore } from "@firebase/firestore";
-import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAKxsD2UV4mToHl4gDHGVKnDO5AFo04DeI",
-  authDomain: "techfest-firebase.firebaseapp.com",
-  projectId: "techfest-firebase",
-  storageBucket: "techfest-firebase.firebasestorage.app",
-  messagingSenderId: "376759457001",
-  appId: "1:376759457001:web:b2af622bdd1f3f12148c78"
+  apiKey: "AIzaSyCc4pmydkKcWvMWtdL_3POMcxuatGYavQ4",
+  authDomain: "techfest-8f66a.firebaseapp.com",
+  projectId: "techfest-8f66a",
+  storageBucket: "techfest-8f66a.firebasestorage.app",
+  messagingSenderId: "530505669745",
+  appId: "1:530505669745:web:977ac87d907293fc8e0fd0"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
